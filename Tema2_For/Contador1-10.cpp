8e8f73df192bc48ba74c12b6952ffd386650e1a6
RodJMA <00110125@uca.edu.sv>
2025-06-01 12:00:28 -0600

    1. Contador del 1 al 10

--- Tema2_For/Contador1-10.cpp
+++ Tema2_For/Contador1-10.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main()
 {
     cout << "Numeros del 1 al 10" << endl;
+    // Iniciar en 1 y parar hasta que sea menor igual que 10, aumentado en uno en uno cada ciclo
     for (int i = 1; i <= 10; i++)
     {
+        // Imprimir 
         cout << i << endl;
     }
 
