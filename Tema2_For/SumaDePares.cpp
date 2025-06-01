94e658398349935570ce4cf74e5d07f91096c875
RodJMA <00110125@uca.edu.sv>
2025-06-01 13:34:06 -0600

    6. Cuenta Regresiva

---
+++ Tema2_For/SumaDePares.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, suma = 0;
+    cout << "Suma de Numeros Pares" << endl;
+    cout << "Ingrese hasta que numero desea sumar: " << "";
+    cin >> n;
+    // Recorrer hasta el numero que ingreso el usuario
+    for (int i = 1; i <= n; i++)
+    {
+        // Verificar si el numero por el que va el bucle es par
+        if ((i % 2) == 0)
+        {
+            // Si el numero es para almacenar el dato y sumarlo con el siguiente
+            suma += i;
+        }
+    }
+    cout << "La suma de los numeros pares hasta " << n << " es: " << suma << endl;
+
+    return 0;
+}
