5d843ce61d1f3375082d3fe03e62db1188837002
RodJMA <00110125@uca.edu.sv>
2025-06-03 21:32:33 -0600

    16. Aproximación de Pi

--- Tema2_For/AproximacionPi.cpp
+++ Tema2_For/AproximacionPi.cpp
@@ -4,19 +4,24 @@ using namespace std;
 
 int main()
 {
-    int terminos;
-    double aprox, sumatoria = 0, resultado;
+    int terminos;                           // Variable para almacenar cuántos términos usará el usuario en la serie
+    double aprox, sumatoria = 0, resultado; // Variables para el término actual, la suma acumulada y el resultado final
+
     cout << "Aproximacion del numero Pi" << endl;
     cout << "Cuantos terminos de la serie deseas usar para aproximar Pi?" << endl;
-    cin >> terminos;
+    cin >> terminos; // El usuario ingresa la cantidad de términos a usar
+
+    // Bucle que recorre desde i = 0 hasta i < terminos
+    // Calcula cada término de la serie de Leibniz: (-1)^i / (2*i + 1)
     for (int i = 0; i < terminos; i++)
     {
-        aprox = pow((-1), i) / ((2 * i) + 1);
-        sumatoria += aprox;
+        aprox = pow((-1), i) / ((2 * i) + 1); // Alterna el signo usando pow y calcula el valor del término
+        sumatoria += aprox;                   // Suma el término al acumulador
     }
-    resultado = 4.0 * sumatoria;
 
-    cout << "La aproximacion de Pi es: " << resultado << endl;
+    resultado = 4.0 * sumatoria; // Multiplica la sumatoria por 4 para obtener la aproximación de π
 
-    return 0;
+    cout << "La aproximacion de Pi es: " << resultado << endl; // Muestra el resultado
+
+    return 0; 
 }
