edbacb47dcaebe1e57c23d03d1a4ad62f727fbe2
RodJMA <00110125@uca.edu.sv>
2025-06-05 17:01:46 -0600

    6 Suma de Números Pares

--- Tema2_For/SumaDePares.cpp
+++ Tema2_For/SumaDePares.cpp
@@ -7,17 +7,25 @@ int main()
     cout << "Suma de Numeros Pares" << endl;
     cout << "Ingrese hasta que numero desea sumar: " << "";
     cin >> n;
-    // Recorrer hasta el numero que ingreso el usuario
-    for (int i = 1; i <= n; i++)
+    if (n > 0)
     {
-        // Verificar si el numero por el que va el bucle es par
-        if ((i % 2) == 0)
+
+        // Recorrer hasta el numero que ingreso el usuario
+        for (int i = 1; i <= n; i++)
         {
-            // Si el numero es par, almacenar el dato y sumarlo con el siguiente
-            suma += i;
+            // Verificar si el numero por el que va el bucle es par
+            if ((i % 2) == 0)
+            {
+                // Si el numero es par, almacenar el dato y sumarlo con el siguiente
+                suma += i;
+            }
         }
+        cout << "La suma de los numeros pares hasta " << n << " es: " << suma << endl;
     }
-    cout << "La suma de los numeros pares hasta " << n << " es: " << suma << endl;
+    else
+    {
+        cout << "Entrada de datos invalida" << endl;
+    }
 
     return 0;
 }
